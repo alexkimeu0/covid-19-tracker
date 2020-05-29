04fdd0c042ea0e21c54d4422f430f1ddadb44942
alexkimeu0 <kim3ual3x@gmail.com>
2020-05-29 23:52:57 +0300

    pulled and displayed countries' data

--- js/main.js
+++ js/main.js
@@ -1,12 +1,264 @@
 const home_url = "https://api.covid19api.com";
 const countries_url = "https://api.covid19api.com/countries";
 const country_url = "https://api.covid19api.com/live/country/";
-let countries_slugs = [];
+const slugs = [
+  "Afghanistan",
+  "Aland Islands",
+  "Albania",
+  "Algeria",
+  "American Samoa",
+  "Andorra",
+  "Angola",
+  "Anguilla",
+  "Antarctica",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bermuda",
+  "Bhutan",
+  "Bolivia",
+  "Bonaire, Sint Eustatius and Saba",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Bouvet Island",
+  "Brazil",
+  "British Indian Ocean Territory",
+  "United States Minor Outlying Islands",
+  "Virgin Islands",
+  "Brunei Darussalam",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cabo Verde",
+  "Cayman Islands",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Christmas Island",
+  "Cocos Islands",
+  "Colombia",
+  "Comoros",
+  "Congo",
+  "DRC",
+  "Cook Islands",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "CuraCao",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Ethiopia",
+  "Falkland Islands",
+  "Faroe Islands",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Guiana",
+  "French Polynesia",
+  "French Southern Territories",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Gibraltar",
+  "Greece",
+  "Greenland",
+  "Grenada",
+  "Guadeloupe",
+  "Guam",
+  "Guatemala",
+  "Guernsey",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Heard Island and McDonald Islands",
+  "Holy See",
+  "Honduras",
+  "Hong Kong",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Cote d'Ivoire",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Isle of Man",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jersey",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Lao People's Democratic Republic",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macao",
+  "Macedonia",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Martinique",
+  "Mauritania",
+  "Mauritius",
+  "Mayotte",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Montserrat",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "Niue",
+  "Norfolk Island",
+  "North Korea",
+  "Northern Mariana Islands",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine, State of",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Pitcairn",
+  "Poland",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Kosovo",
+  "Reunion",
+  "Romania",
+  "Russian Federation",
+  "Rwanda",
+  "Saint Barthelemy",
+  "Saint Helena, Ascension and Tristan da Cunha",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Martin",
+  "Saint Pierre and Miquelon",
+  "Saint Vincent and the Grenadines",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Sint Maarten",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Georgia and the South Sandwich Islands",
+  "South Korea",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Svalbard and Jan Mayen",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syrian Arab Republic",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania, United Republic of",
+  "Thailand",
+  "Timor-Leste",
+  "Togo",
+  "Tokelau",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Turks and Caicos Islands",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom of Great Britain and Northern Ireland",
+  "United States of America",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Venezuela",
+  "Viet Nam",
+  "Wallis and Futuna",
+  "Western Sahara",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
+
+const counties = [];
 
 $(document).ready(() => {
-  getData();
-  getCountries();
   getCountryData();
+  getData();
+  getFooter();
 });
 
 function getData() {
@@ -14,8 +266,6 @@ function getData() {
     .get(`${home_url}/summary`)
     .then((res) => {
       let global = res.data.Global;
-      console.log(global);
-
       let output = `
         <div class="col-md-12">
             <div class="card">
@@ -55,21 +305,49 @@ function getData() {
     .catch((err) => console.log(err));
 }
 
-function getCountries() {
-  axios.get(`${countries_url}`).then((res) => {
-    let countries = res.data;
+function getCountryData() {
+  let output = "";
+  for (let i = 0; i < slugs.sort().length; i++) {
+    axios
+      .get(
+        `https://shrouded-harbor-39689.herokuapp.com/https://covid19-stats-api.herokuapp.com/api/v1/cases?country=${slugs[i]}`
+      )
+      .then((res) => {
+        let countries_data = res.data;
 
-    for (let i = 0; i < countries.length; i++) {
-      let slug = countries[i].Slug;
-    }
-  });
+        //console.log(countries_data);
+
+        output += `
+          <div class="col-md-4">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="text-center"><strong>Country:</strong>&nbsp;${slugs[i]}</h5>
+              </div>  
+              <div class="card-body">
+              <ul class="list-group">
+                  <li class="list-group-item"><strong>Confirmed:</strong>&nbsp;${countries_data.confirmed}</li>
+                  <li class="list-group-item"><strong>Deaths:</strong>&nbsp;${countries_data.deaths}</li>
+                  <li class="list-group-item"><strong>Recovered:</strong>&nbsp;${countries_data.recovered}</li>                                
+              </ul>
+
+              </div>          
+            </div>
+          </div>          
+          `;
+
+        $("#countries").html(output);
+      });
+  }
 }
 
-function getCountryData(slug) {
-  axios.get(`https://api.covid19api.com/total/country/${slug}`).then((res) => {
-    let output = "";
-    let data = res.data;
-  });
+function getFooter() {
+  let output = `<p><span><i class="fas fa-exclamation-triangle fa-2x"></i></span><br>I realized the data pulled from the API I used is not upto date.
+  <br>
+  I might consider looking for an upto date API later. Also not all countries are presented. 
+  <br>
+  But for now, this is good enough.
+  <br>
+  I just did this for fun-:)<br>
+  Alex&nbsp;<i class="fas fa-heart"></i></p>`;
+  $("#footer").html(output);
 }
-
-//console.log(countries_slugs);
